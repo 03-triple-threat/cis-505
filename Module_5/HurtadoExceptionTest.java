93942faec65e29d201024db25827364ed4d7ea1b
AJ Hurtado <ajhurtado3@gmail.com>
2021-07-04 00:49:47 -0500

    Assignment 5.1 - Program 1

--- Module_5/HurtadoExceptionTest.java
+++ Module_5/HurtadoExceptionTest.java
@@ -1,3 +1,8 @@
+/**
+ * Liang, Y.D. (2019). Introduction to Java Programming and Data Structures:
+      Comprehensive Version (12th ed.). Pearson Education, Inc.
+    Modified by A. Hurtado 2021
+ */
 package Module_5;
 
 import java.util.ArrayList;
@@ -7,8 +12,14 @@ public class HurtadoExceptionTest {
 
     public static void main(String[] args) {
 
+        /**
+         * Instantiates a new ArrayList of the String type
+         */
         ArrayList <String> newList = new ArrayList<>();
 
+        /**
+         * Addition of a ten Strings to the newlist ArrayList
+         */
         newList.add("Bellevue");
         newList.add("University");
         newList.add("Summer");
@@ -20,16 +31,24 @@ public class HurtadoExceptionTest {
         newList.add("OOP");
         newList.add("Online");
 
+        /**
+         * ForEach loop that prints out each element within the newList ArrayList 
+         */
         for (String s: newList) {
             System.out.println(s);
         }
 
         System.out.println("\nWhich element of the ArrayList would you like to see?");
 
+        /**
+         * Used for user input
+         */
         Scanner input = new Scanner(System.in);
 
-        // int inputNumber;// = input.nextInt();
-
+        /**
+         * try-catch block that will print out an element within the ArrayList so long as 
+         * the user inputs a valid int. 
+         */
         try {
             int inputNumber = input.nextInt();
             System.out.println("\n" + newList.get(inputNumber) + "\n");
---
+++ Module_5/HurtadoStreamsTest.java
@@ -0,0 +1,5 @@
+package Module_5;
+
+public class HurtadoStreamsTest {
+    
+}
